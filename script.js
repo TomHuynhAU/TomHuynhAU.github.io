6dab587da7947acb26f2623aef32114866482151
quoct <quocthong05092002@gmail.com>
2025-04-11 02:31:50 +1000

    Complete Loading

--- script.js
+++ script.js
@@ -2,5 +2,5 @@ window.addEventListener("load", function () {
   const loader = document.getElementById("loading");
   setTimeout(() => {
     loader.style.display = "none";
-  }, 3000); // 3000ms = 3 giây
+  }, 6000); // 3000ms = 3 giây
 });
