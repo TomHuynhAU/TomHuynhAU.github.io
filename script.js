aee264a3ac0cdf79818e77485bce9a67f070427f
TomHuynh <89759835+TomHuynhAU@users.noreply.github.com>
2025-04-11 02:11:31 +1000

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", function () {
-  const loader = document.getElementById("loading");
-  setTimeout(() => {
-    loader.style.display = "none";
-  }, 3000); // 3000ms = 3 giây
-});
