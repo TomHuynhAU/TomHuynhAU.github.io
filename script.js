84e5e54df2288e85dc3705b9ab83ec5d7d6f8346
quoct <quocthong05092002@gmail.com>
2025-04-15 12:30:58 +1000

    optimize UI and remove loading

--- script.js
+++ script.js
@@ -1,34 +1,6 @@
 document.addEventListener("DOMContentLoaded", () => {
   const navbar = document.querySelector(".navbar");
   const info2 = document.querySelector("#info2");
-  const loading = document.getElementById("loading");
-  const loadingPercentage = document.getElementById("loading-percentage");
-  const loadingProgress = document.querySelector(".loading-progress");
-
-  let progress = 0;
-
-  // Ẩn thanh cuộn khi loading bắt đầu
-  document.body.style.overflow = "hidden";
-
-  // Tăng dần phần trăm và chiều rộng thanh loading
-  const interval = setInterval(() => {
-    progress += 1;
-    loadingPercentage.textContent = `${progress}%`;
-    loadingProgress.style.width = `${progress}%`;
-
-    // Khi đạt 100%, ẩn màn hình loading
-    if (progress >= 100) {
-      clearInterval(interval);
-      setTimeout(() => {
-        loading.classList.add("hidden"); // Thêm lớp hidden để mờ dần
-        setTimeout(() => {
-          loading.style.display = "none"; // Ẩn hoàn toàn sau khi mờ dần
-          // Hiển thị lại thanh cuộn sau khi loading hoàn tất
-          document.body.style.overflow = "";
-        }, 500); // Thời gian khớp với transition trong CSS
-      }, 500); // Thêm 0.5 giây để hoàn tất hiệu ứng
-    }
-  }, 30); // Cập nhật mỗi 30ms
 
   window.addEventListener("scroll", () => {
     if (!info2) return;
