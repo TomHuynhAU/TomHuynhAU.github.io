a44e9197ec21d008c658454a180e18ca2672b296
quoct <quocthong05092002@gmail.com>
2025-04-16 15:54:09 +1000

    complete #2

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 document.addEventListener("DOMContentLoaded", () => {
   const navbar = document.querySelector(".navbar");
   const info2 = document.querySelector("#info2");
+  const background = document.getElementById("background");
+  const img = new Image();
+  img.src = "./assets/background.jpg"; // Đường dẫn ảnh nền
+  img.onload = () => {
+    background.style.backgroundImage = `url(${img.src})`;
+  };
 
   window.addEventListener("scroll", () => {
     if (!info2) return;
