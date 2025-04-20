99ea5773586c6ac5923667bf92f7757315caa476
TomHuynh <89759835+TomHuynhAU@users.noreply.github.com>
2025-04-20 21:07:21 +1000

    Delete steamauth directory

--- steamauth/SteamConfig.php
+++
@@ -1,7 +0,0 @@
-<?php
-$steamauth['apikey'] = "67080849D059BF2A4E8408460E1F680E"; // Đăng ký tại https://steamcommunity.com/dev/apikey
-$steamauth['domainname'] = "http://localhost"; // hoặc tên miền bạn đang dùng
-$steamauth['loginpage'] = "../index.php";
-$steamauth['logoutpage'] = "../index.php";
-$steamauth['db_connection'] = false;
-?>
--- steamauth/login.php
+++
@@ -1,5 +0,0 @@
-<?php
-require 'SteamAuth.class.php';
-$auth = new SteamAuth();
-$auth->redirect();
-
